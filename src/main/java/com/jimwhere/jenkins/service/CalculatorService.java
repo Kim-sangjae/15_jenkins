af2235f2d55b47644ecd1e5da014299f64199f38
Kim-sangjae <kimsangjae94114@gmail.com>
2025-12-08 11:29:38 +0900

    커밋

---
+++ src/main/java/com/jimwhere/jenkins/service/CalculatorService.java
@@ -0,0 +1,12 @@
+package com.jimwhere.jenkins.service;
+
+
+import com.jimwhere.jenkins.dto.CalculatorDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorService {
+    public int plusTwoNumbers(CalculatorDTO calculatorDTO) {
+        return calculatorDTO.getNum1() + calculatorDTO.getNum2();
+    }
+}
